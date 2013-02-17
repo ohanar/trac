2732a0d5e51e8fda612b163b2076df118964b9b3
Christian Boos <cboos@edgewall.org>
2013-02-17 11:43:41 +0100

    pygit2: show libgit2 version (experimental)

--- tracopt/versioncontrol/git/git_fs.py
+++ tracopt/versioncontrol/git/git_fs.py
@@ -158,6 +158,9 @@ class GitConnector(Component):
     def get_system_info(self):
         if pygit2:
             yield 'pygit2', pygit2.__version__
+            libgit2_version = getattr(pygit2, '__libgit2_version__', None)
+            if libgit2_version:
+                yield 'libgit2', libgit2_version
 
     # IWikiSyntaxProvider methods
 
